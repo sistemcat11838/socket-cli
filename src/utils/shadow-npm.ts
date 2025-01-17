e19fdcba6781188c2796bc9fea04e5118c93bab5
jdalton <john.david.dalton@gmail.com>
2025-01-16 22:11:08 -0500

    Fix fork use in shadow-npm

--- src/utils/shadow-npm.ts
+++ src/utils/shadow-npm.ts
@@ -5,25 +5,25 @@ import { fork } from './promise-fork'
 import constants from '../constants'
 
 import type { ForkOptions, ForkResult } from './promise-fork'
+import type { Serializable } from 'node:child_process'
 
 const { abortSignal } = constants
 
 type ShadowNpmInstallOptions = ForkOptions & {
   flags?: string[]
+  ipc?: Serializable
 }
 
 export function shadowNpmInstall<O extends ShadowNpmInstallOptions>(
   opts?: ShadowNpmInstallOptions
 ) {
-  const { flags = [], ...spawnOptions } = { __proto__: null, ...opts }
+  const { flags = [], ipc, ...forkOptions } = { __proto__: null, ...opts }
   // Lazily access constants.ENV.
   const { SOCKET_CLI_DEBUG } = constants.ENV
-  return fork(
-    // Lazily access constants.execPath.
-    constants.execPath,
+  const promise = fork(
+    // Lazily access constants.rootBinPath.
+    path.join(constants.rootBinPath, 'npm-cli.js'),
     [
-      // Lazily access constants.rootBinPath.
-      path.join(constants.rootBinPath, 'npm-cli.js'),
       'install',
       // Even though the 'silent' flag is passed npm will still run through code
       // paths for 'audit' and 'fund' unless '--no-audit' and '--no-fund' flags
@@ -37,11 +37,15 @@ export function shadowNpmInstall<O extends ShadowNpmInstallOptions>(
       signal: abortSignal,
       // Lazily access constants.ENV.
       stdio: SOCKET_CLI_DEBUG ? 'inherit' : 'ignore',
-      ...spawnOptions,
+      ...forkOptions,
       env: {
         ...process.env,
-        ...spawnOptions.env
+        ...forkOptions.env
       }
     }
   ) as ForkResult<O extends { stdioString: false } ? Buffer : string, undefined>
+  if (ipc) {
+    promise.process.send(ipc)
+  }
+  return promise
 }
