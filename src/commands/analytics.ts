649ced6f7056ad1cdf53a38e481e7947d7661edf
jdalton <john.david.dalton@gmail.com>
2025-02-06 11:41:38 -0500

    Cleanup analytics command

--- src/commands/analytics.ts
+++ src/commands/analytics.ts
@@ -168,49 +168,9 @@ const METRICS = [
   'total_high_prevented',
   'total_medium_prevented',
   'total_low_prevented'
-]
+] as const
 
-async function fetchOrgAnalyticsData(
-  time: number,
-  spinner: Spinner,
-  apiToken: string,
-  outputJson: boolean,
-  filePath: string
-): Promise<void> {
-  const socketSdk = await setupSdk(apiToken)
-  const result = await handleApiCall(
-    socketSdk.getOrgAnalytics(time.toString()),
-    'fetching analytics data'
-  )
-
-  if (result.success === false) {
-    return handleUnsuccessfulApiResponse('getOrgAnalytics', result, spinner)
-  }
-
-  spinner.stop()
-
-  if (!result.data.length) {
-    return console.log(
-      'No analytics data is available for this organization yet.'
-    )
-  }
-  const data = formatData(result.data, 'org')
-  if (outputJson && !filePath) {
-    return console.log(result.data)
-  }
-  if (filePath) {
-    try {
-      await fs.writeFile(filePath, JSON.stringify(result.data), 'utf8')
-      console.log(`Data successfully written to ${filePath}`)
-    } catch (e: any) {
-      console.error(e)
-    }
-    return
-  }
-  return displayAnalyticsScreen(data)
-}
-
-const months = [
+const MONTHS = [
   'Jan',
   'Feb',
   'Mar',
@@ -223,159 +183,9 @@ const months = [
   'Oct',
   'Nov',
   'Dec'
-]
+] as const
 
-const formatDate = (date: string) => {
-  return `${months[new Date(date).getMonth()]} ${new Date(date).getDate()}`
-}
-
-const formatData = (data: any, scope: string) => {
-  let formattedData, sortedTopFiveAlerts
-
-  if (scope === 'org') {
-    const topFiveAlerts = data.map(
-      (d: { [k: string]: any }) => d['top_five_alert_types']
-    )
-
-    const totalTopAlerts: { [key: string]: number } = topFiveAlerts.reduce(
-      (acc: { [k: string]: number }, current: { [key: string]: number }) => {
-        const alertTypes = Object.keys(current)
-        alertTypes.map((type: string) => {
-          if (!acc[type]) {
-            acc[type] = current[type]!
-          } else {
-            acc[type] += current[type]!
-          }
-          return acc
-        })
-        return acc
-      },
-      {} as { [k: string]: number }
-    )
-
-    sortedTopFiveAlerts = Object.entries(totalTopAlerts)
-      .sort(({ 1: a }, { 1: b }) => b - a)
-      .slice(0, 5)
-      .reduce(
-        (r, { 0: k, 1: v }) => {
-          r[k] = v
-          return r
-        },
-        {} as typeof totalTopAlerts
-      )
-
-    const formatData = (label: string) => {
-      return data.reduce(
-        (acc: { [k: string]: number }, current: { [key: string]: any }) => {
-          const date: string = formatDate(current['created_at'])
-          if (!acc[date]) {
-            acc[date] = current[label]!
-          } else {
-            acc[date] += current[label]!
-          }
-          return acc
-        },
-        {}
-      )
-    }
-
-    formattedData = METRICS.reduce(
-      (acc, current: string) => {
-        acc[current] = formatData(current)
-        return acc
-      },
-      {} as { [k: string]: number }
-    )
-  } else if (scope === 'repo') {
-    const topAlerts: { [key: string]: number } = data.reduce(
-      (acc: { [key: string]: number }, current: { [key: string]: any }) => {
-        const alertTypes = Object.keys(current['top_five_alert_types'])
-        alertTypes.map(type => {
-          if (!acc[type]) {
-            acc[type] = current['top_five_alert_types'][type]
-          } else {
-            if (current['top_five_alert_types'][type] > (acc[type] || 0)) {
-              acc[type] = current['top_five_alert_types'][type]
-            }
-          }
-          return acc
-        })
-        return acc
-      },
-      {} as { [key: string]: number }
-    )
-
-    sortedTopFiveAlerts = Object.entries(topAlerts)
-      .sort(({ 1: a }, { 1: b }) => b - a)
-      .slice(0, 5)
-      .reduce(
-        (r, { 0: k, 1: v }) => {
-          r[k] = v
-          return r
-        },
-        {} as typeof topAlerts
-      )
-
-    formattedData = data.reduce(
-      (acc: any, current: { [key: string]: any }) => {
-        METRICS.forEach((m: string) => {
-          if (!acc[m]) {
-            acc[m] = {}
-          }
-          acc[m][formatDate(current['created_at'])] = current[m]
-          return acc
-        })
-        return acc
-      },
-      {} as { [k: string]: number }
-    )
-  }
-
-  return { ...formattedData, top_five_alert_types: sortedTopFiveAlerts }
-}
-
-async function fetchRepoAnalyticsData(
-  repo: string,
-  time: number,
-  spinner: Spinner,
-  apiToken: string,
-  outputJson: boolean,
-  filePath: string
-): Promise<void> {
-  const socketSdk = await setupSdk(apiToken)
-  const result = await handleApiCall(
-    socketSdk.getRepoAnalytics(repo, time.toString()),
-    'fetching analytics data'
-  )
-
-  if (result.success === false) {
-    return handleUnsuccessfulApiResponse('getRepoAnalytics', result, spinner)
-  }
-
-  spinner.stop()
-
-  if (!result.data.length) {
-    return console.log(
-      'No analytics data is available for this organization yet.'
-    )
-  }
-  const data = formatData(result.data, 'repo')
-  if (outputJson && !filePath) {
-    return console.log(result.data)
-  }
-  if (filePath) {
-    try {
-      await fs.writeFile(filePath, JSON.stringify(result.data), 'utf8')
-      console.log(`Data successfully written to ${filePath}`)
-    } catch (e: any) {
-      console.error(e)
-    }
-    return
-  }
-  return displayAnalyticsScreen(data)
-}
-
-const displayAnalyticsScreen = (data: any) => {
+function displayAnalyticsScreen(data: any) {
   const screen = new ScreenWidget()
   const grid = new GridLayout({ rows: 5, cols: 4, screen })
 
@@ -457,13 +267,181 @@ const displayAnalyticsScreen = (data: any) => {
   screen.key(['escape', 'q', 'C-c'], () => process.exit(0))
 }
 
-const renderLineCharts = (
+async function fetchOrgAnalyticsData(
+  time: number,
+  spinner: Spinner,
+  apiToken: string,
+  outputJson: boolean,
+  filePath: string
+): Promise<void> {
+  const socketSdk = await setupSdk(apiToken)
+  const result = await handleApiCall(
+    socketSdk.getOrgAnalytics(time.toString()),
+    'fetching analytics data'
+  )
+
+  if (result.success === false) {
+    return handleUnsuccessfulApiResponse('getOrgAnalytics', result, spinner)
+  }
+
+  spinner.stop()
+
+  if (!result.data.length) {
+    return console.log(
+      'No analytics data is available for this organization yet.'
+    )
+  }
+  const data = formatData(result.data, 'org')
+  if (outputJson && !filePath) {
+    console.log(result.data)
+    return
+  }
+  if (filePath) {
+    try {
+      await fs.writeFile(filePath, JSON.stringify(result.data), 'utf8')
+      console.log(`Data successfully written to ${filePath}`)
+    } catch (e: any) {
+      console.error(e)
+    }
+    return
+  }
+  return displayAnalyticsScreen(data)
+}
+
+async function fetchRepoAnalyticsData(
+  repo: string,
+  time: number,
+  spinner: Spinner,
+  apiToken: string,
+  outputJson: boolean,
+  filePath: string
+): Promise<void> {
+  const socketSdk = await setupSdk(apiToken)
+  const result = await handleApiCall(
+    socketSdk.getRepoAnalytics(repo, time.toString()),
+    'fetching analytics data'
+  )
+
+  if (result.success === false) {
+    return handleUnsuccessfulApiResponse('getRepoAnalytics', result, spinner)
+  }
+
+  spinner.stop()
+
+  if (!result.data.length) {
+    return console.log(
+      'No analytics data is available for this organization yet.'
+    )
+  }
+  const data = formatData(result.data, 'repo')
+  if (outputJson && !filePath) {
+    return console.log(result.data)
+  }
+  if (filePath) {
+    try {
+      await fs.writeFile(filePath, JSON.stringify(result.data), 'utf8')
+      console.log(`Data successfully written to ${filePath}`)
+    } catch (e: any) {
+      console.error(e)
+    }
+    return
+  }
+  return displayAnalyticsScreen(data)
+}
+
+type FormattedData = {
+  top_five_alert_types: { [key: string]: number }
+  total_critical_alerts: { [key: string]: number }
+  total_high_alerts: { [key: string]: number }
+  total_medium_alerts: { [key: string]: number }
+  total_low_alerts: { [key: string]: number }
+  total_critical_added: { [key: string]: number }
+  total_medium_added: { [key: string]: number }
+  total_low_added: { [key: string]: number }
+  total_high_added: { [key: string]: number }
+  total_critical_prevented: { [key: string]: number }
+  total_high_prevented: { [key: string]: number }
+  total_medium_prevented: { [key: string]: number }
+  total_low_prevented: { [key: string]: number }
+}
+
+function formatData(
+  data: { [key: string]: any }[],
+  scope: string
+): FormattedData {
+  const formattedData = <Omit<FormattedData, 'top_five_alert_types'>>{}
+  const sortedTopFiveAlerts: { [key: string]: number } = {}
+  const totalTopAlerts: { [key: string]: number } = {}
+
+  for (const metric of METRICS) {
+    formattedData[metric] = {}
+  }
+  if (scope === 'org') {
+    for (const entry of data) {
+      const topFiveAlertTypes = entry['top_five_alert_types']
+      for (const type of Object.keys(topFiveAlertTypes)) {
+        const count = topFiveAlertTypes[type] ?? 0
+        if (!totalTopAlerts[type]) {
+          totalTopAlerts[type] = count
+        } else {
+          totalTopAlerts[type] += count
+        }
+      }
+    }
+    for (const metric of METRICS) {
+      const formatted = formattedData[metric]
+      for (const entry of data) {
+        const date = formatDate(entry['created_at'])
+        if (!formatted[date]) {
+          formatted[date] = entry[metric]!
+        } else {
+          formatted[date] += entry[metric]!
+        }
+      }
+    }
+  } else if (scope === 'repo') {
+    for (const entry of data) {
+      const topFiveAlertTypes = entry['top_five_alert_types']
+      for (const type of Object.keys(topFiveAlertTypes)) {
+        const count = topFiveAlertTypes[type] ?? 0
+        if (!totalTopAlerts[type]) {
+          totalTopAlerts[type] = count
+        } else if (count > (totalTopAlerts[type] ?? 0)) {
+          totalTopAlerts[type] = count
+        }
+      }
+    }
+    for (const entry of data) {
+      for (const metric of METRICS) {
+        formattedData[metric]![formatDate(entry['created_at'])] = entry[metric]
+      }
+    }
+  }
+
+  const topFiveAlertEntries = Object.entries(totalTopAlerts)
+    .sort(({ 1: a }, { 1: b }) => b - a)
+    .slice(0, 5)
+  for (const { 0: key, 1: value } of topFiveAlertEntries) {
+    sortedTopFiveAlerts[key] = value
+  }
+
+  return {
+    ...formattedData,
+    top_five_alert_types: sortedTopFiveAlerts
+  }
+}
+
+function formatDate(date: string) {
+  return `${MONTHS[new Date(date).getMonth()]} ${new Date(date).getDate()}`
+}
+
+function renderLineCharts(
   grid: any,
   screen: any,
   title: string,
   coords: number[],
   data: { [key: string]: number }
-) => {
+) {
   const line = grid.set(...coords, LineChart, {
     style: { line: 'cyan', text: 'cyan', baseline: 'black' },
     xLabelPadding: 0,
